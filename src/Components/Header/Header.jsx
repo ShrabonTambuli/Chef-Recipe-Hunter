78a60555eef44b5f2317479ba5e792feef9d5a69
ShrabonTambuli <shrabonsa@gmail.com>
2023-05-02 00:21:17 +0600

    Added Navbar

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,28 +4,25 @@ import { NavLink } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <nav className='grid grid-cols-3 gap-4 px-24 mt-9'>
+        <nav className='grid grid-cols-3 items-center gap-4 px-24 mt-9'>
             <div>
                 <img src="/images/logo.png" alt="" />
             </div>
-            <div>
+            <div className='mx-auto'>
                 <ul className='flex font-serif'>
                     <li>
                         <NavLink to='/home' className={({ isActive }) => (isActive ? 'text-orange-600' : '')}>Home</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to='/statistics' className={({ isActive }) => (isActive ? 'text-orange-600' : '')}>Statistics</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to='/applied-jobs' className={({ isActive }) => (isActive ? 'text-orange-600' : '')}>Applied Jobs</NavLink>
                     </li>
                     <li>
                         <NavLink to='/blog' className={({ isActive }) => (isActive ? 'text-orange-600' : '')}>Blog</NavLink>
                     </li>
                 </ul>
             </div>
-            <div className='text-center'>
-                <button className='btns font-serif'>Star Applying</button>
+            <div className='mx-auto inline-flex items-center'>
+                <div className='font-semibold font-serif text-xl'>
+                    <NavLink>LogIn</NavLink>
+                </div>
+                <img className='w-24 h-24 rounded-full ms-6' src="/images/Chef.jpg" alt="" />
             </div>
         </nav>
     );
